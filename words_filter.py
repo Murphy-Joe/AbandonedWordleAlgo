d0cf31d00bee92948f453b0eb3a4380588e843da
Joe Murphy <murphcle@gmail.com>
2022-03-28 10:54:41 -0400

    switching to static words filter

--- words_filter.py
+++ words_filter.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 
 class WordsFilter:
     def __init__(self,
-                 excluded_letters: list[str] = [],
+                 excluded_letters: set[str] = set(),
                  included_letters: list[str] = [],
                  index_excludes_letters: dict[int, list[str]] = {},
                  indexed_letters: dict[int, str] = {}):
@@ -12,8 +12,10 @@ class WordsFilter:
         self.IndexExcludesLetters = index_excludes_letters
         self.IndexedLetters = indexed_letters
 
-    def get_new_filter(self, guess: str, target: str) -> WordsFilter:
-        excluded_letters, included_letters = [], []
+    @staticmethod
+    def get_new_filter(guess: str, target: str) -> WordsFilter:
+        excluded_letters = set()
+        included_letters = []
         index_excludes_letters, indexed_letters = {}, {}
 
         for i, letter in enumerate(guess):
@@ -24,42 +26,43 @@ class WordsFilter:
                 index_excludes_letters[i] = [letter]
                 included_letters.append(letter)
             elif letter not in included_letters and letter not in indexed_letters.values():
-                excluded_letters.append(letter)
+                excluded_letters.add(letter)
         turn_filter = WordsFilter(
             excluded_letters, included_letters, index_excludes_letters, indexed_letters)
         return turn_filter
 
-    def update_filter(self, new_filter: WordsFilter) -> WordsFilter:
+    @staticmethod
+    def merge_filters(existing_filter: WordsFilter, new_filter: WordsFilter) -> WordsFilter:
 
         # exluded letters
-        self.ExcludedLetters.extend(new_filter.ExcludedLetters)
+        existing_filter.ExcludedLetters.update(new_filter.ExcludedLetters)
 
         # indexed letters
         for k, v in new_filter.IndexedLetters.items():
-            self.IndexedLetters.setdefault(k, v)
+            existing_filter.IndexedLetters.setdefault(k, v)
 
         # index excludes letters
         for idx, letter_list in new_filter.IndexExcludesLetters.items():
-            if idx in self.IndexExcludesLetters.keys() \
-                    and letter_list[0] not in self.IndexExcludesLetters[idx]:
-                self.IndexExcludesLetters[idx].extend(letter_list)
+            if idx in existing_filter.IndexExcludesLetters.keys() \
+                    and letter_list[0] not in existing_filter.IndexExcludesLetters[idx]:
+                existing_filter.IndexExcludesLetters[idx].extend(letter_list)
             else:
-                self.IndexExcludesLetters[idx] = letter_list
+                existing_filter.IndexExcludesLetters[idx] = letter_list
 
         # included letters
         for letter in new_filter.IncludedLetters:
-            if letter in self.IncludedLetters:
-                self.IncludedLetters.remove(letter)
-        self.IncludedLetters.extend(new_filter.IncludedLetters)
+            if letter in existing_filter.IncludedLetters:
+                existing_filter.IncludedLetters.remove(letter)
+        existing_filter.IncludedLetters.extend(new_filter.IncludedLetters)
 
-        return self
+        return existing_filter
 
 
 if __name__ == '__main__':
-    wf1 = WordsFilter()
-    wf2 = WordsFilter()
-    guess_filter1 = wf1.get_new_filter('coast', 'sunny')
-    print(guess_filter1.__dict__)
-    guess_filter2 = wf1.get_new_filter('nanns', 'sunny')
-    guess_filter1.update_filter(guess_filter2)
-    print(guess_filter1.__dict__)
+    guess_filter1 = WordsFilter.get_new_filter('coast', 'sunny')
+    print('\n1st guess \n', guess_filter1.__dict__)
+    guess_filter2 = WordsFilter.get_new_filter('nanns', 'sunny')
+    print('\n2nd guess \n', guess_filter2.__dict__)
+    guess_filter_merged = WordsFilter.merge_filters(
+        guess_filter1, guess_filter2)
+    print('\nmerged filter \n', guess_filter_merged.__dict__)
