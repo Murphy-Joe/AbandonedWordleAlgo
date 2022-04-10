cba4c43ce9c0a3259bfe4facaf5527105b0e4ffd
Joe Murphy <murphcle@gmail.com>
2022-04-09 21:05:12 -0400

    working model of parallel micro requests

--- old/coroutine_calls.py
+++ old/coroutine_calls.py
@@ -13,7 +13,7 @@ async def call_all_params(params):
     async with aiohttp.ClientSession() as session:
         tasks = []
         for param in params:
-            task = asyncio.ensure_future(call_param(session, param))
+            task = asyncio.create_task(call_param(session, param))
             tasks.append(task)
         await asyncio.gather(*tasks, return_exceptions=True)
 
@@ -22,19 +22,27 @@ async def call_all_params(params):
 # each game_call gets added as a task
 # not sure what happens on gather to combine dicts
 
-async def words_left_and_guess_words(guess_list: list[str]):
+async def targets_and_guesses(guess_list, session):
+    payload = {"guesses": guess_list}
+    async with session.post('/wordsleft', json=payload) as words_post:
+        return await words_post.json()
+        # print(targets_left_and_best_guesses)
+
+async def guess_score(guess_list, session, next_guess):
+    payload = {"guesses": guess_list, "next_guess": next_guess}
+    async with session.post('/game', json=payload) as game_post:
+        return await game_post.json()
+        # print(guess_dict)
+
+async def runner(guess_list: list[str]):
     async with aiohttp.ClientSession("https://1vv6d7.deta.dev") as session:
-        async with session.post('/wordsleft', json={"guesses": guess_list}) as words_post:
-            targets_left_and_best_guesses = await words_post.json()
-            print(targets_left_and_best_guesses)
-        async with session.post('/game', json={"guesses": guess_list, "next_guess": "whope"}) as game_post:
-            guess_dict = await game_post.json()
-            print(guess_dict)
-
-
-
-
-
+        resp_targets_and_guesses = await targets_and_guesses(guess_list, session)
+        tasks = []
+        for next_guess in resp_targets_and_guesses["bestGuessWords"]:
+            task = asyncio.create_task(guess_score(guess_list, session, next_guess))
+            tasks.append(task)
+        res = await asyncio.gather(*tasks, return_exceptions=True)
+        print(res)
 
 """ async def main():
     # Schedule three calls *concurrently*:
@@ -54,4 +62,4 @@ if __name__ == "__main__":
     duration = time.time() - start_time
     print(f"Called {len(params)} params in {duration} seconds") """
 
-    asyncio.run(words_left_and_guess_words(["scare"]))
+    asyncio.run(runner(["scare"]))
