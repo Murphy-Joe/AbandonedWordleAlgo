390ad434ebbee3f48f13497ee68129e39e703aa3
Joe Murphy <murphcle@gmail.com>
2022-04-21 07:04:06 -0400

    controller clean up

--- main.py
+++ main.py
@@ -44,45 +44,31 @@ async def get_guess_dict(guesses: str = Query(str), nextguess: str = Query(str))
         "data": guessDict
     }
 
-# @app.get("/{item}")
-# async def test(item: str):
-#     await asyncio.sleep(1)
-#     return {
-#         "item": item
-#     }
-
 @app.post("/game")
 async def single_guess_dict(body: Guesses):
-    create_game = time.time()
+    # create_game = time.time()
     game = WordleGame()
-    end_create_game = time.time()
+    # end_create_game = time.time()
 
-    make_guess = time.time()
+    # make_guess = time.time()
     for guess in body.guesses:
         game.make_guess(guess)
-    end_make_guess = time.time()
+    # end_make_guess = time.time()
 
-    create_solver = time.time()
+    # create_solver = time.time()
     solver = Solver(game)
-    end_create_solver = time.time()
+    # end_create_solver = time.time()
 
-    words_left_time = time.time()
+    # words_left_time = time.time()
     words_left = solver.answers_that_meet_criteria(game.ResultsFilter)
-    end_words_left = time.time()
+    # end_words_left = time.time()
 
-    narrowing_scores = time.time()
+    # narrowing_scores = time.time()
     guess_dict = solver.narrowing_score_per_guess_async(body.next_guess, words_left)
-    end_narrowing_scores = time.time()
+    # end_narrowing_scores = time.time()
 
     # return {"create_game": end_create_game - create_game, "make_guess": end_make_guess - make_guess, "create_solver": end_create_solver - create_solver, "words_left": end_words_left - words_left_time, "narrowing_scores": end_narrowing_scores - narrowing_scores, "guess_dict": guess_dict}
     return guess_dict
-
-@app.get("/jsontime")
-async def time_to_load_json():
-    start_time = time.time()
-    with open('words/targets.json', 'r') as answers_json:
-        answers = json.load(answers_json)
-    return time.time() - start_time
 
 if __name__ == "__main__":
     import uvicorn
