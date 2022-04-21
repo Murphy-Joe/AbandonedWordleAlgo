f524752107899903726937bbaad9798cdcd7d9c7
Joe Murphy <murphcle@gmail.com>
2022-04-21 06:43:59 -0400

    cleaned up coroutine demo file

--- old/coroutine_calls.py
+++ old/coroutine_calls.py
@@ -1,27 +1,7 @@
 import asyncio
-import json
-from pprint import pprint
 import time
 import aiohttp
 
-async def call_param(session, param):
-    url = f"https://1vv6d7.deta.dev/{param}"
-    async with session.get(url) as response:
-        print(f"Response: {response.json()} from {param}")
-
-
-async def call_all_params(params):
-    async with aiohttp.ClientSession() as session:
-        tasks = []
-        for param in params:
-            task = asyncio.create_task(call_param(session, param))
-            tasks.append(task)
-        await asyncio.gather(*tasks, return_exceptions=True)
-
-# game_post method extraction similar to call_param method goes here
-# each game_post call will get the session and payload passed to it
-# each game_call gets added as a task
-# not sure what happens on gather to combine dicts
 
 async def targets_and_guesses(guess_list, session):
     payload = {"guesses": guess_list}
@@ -40,7 +20,8 @@ async def runner(guess_list: list[str]):
         resp_targets_and_guesses = await targets_and_guesses(guess_list, session)
         print(f'\nnumber of guesses to check: {len(resp_targets_and_guesses["bestGuessWords"])}')
         print(f'number of targets left: {len(resp_targets_and_guesses["wordsLeft"])}')
-        if len(resp_targets_and_guesses["wordsLeft"]) == 1 and resp_targets_and_guesses["wordsLeft"][0] == guess_list[-1]:
+        if (len(resp_targets_and_guesses["wordsLeft"]) == 1
+        and resp_targets_and_guesses["wordsLeft"][0] == guess_list[-1]):
             print(f'You won in {len(guess_list)} guesses!')
         tasks = []
         for next_guess in resp_targets_and_guesses["bestGuessWords"]:
@@ -50,34 +31,12 @@ async def runner(guess_list: list[str]):
         # res.sort(key=lambda tup: tup[1])
         return res
 
-
-
-""" async def main():
-    # Schedule three calls *concurrently*:
-    L = await asyncio.gather(
-        factorial("A", 2),
-        factorial("B", 3),
-        factorial("C", 4),
-    )
-    print(L) """
-
 if __name__ == "__main__":
-    # params = ["hey", "there", "dude"] * 3
-
-    """ start_time = time.time()
-    asyncio.run(call_all_params(params))
-    # asyncio.get_event_loop().run_until_complete(call_all_params(params))
-    duration = time.time() - start_time
-    print(f"Called {len(params)} params in {duration} seconds") """
 
     start_time = time.time()
-    results = asyncio.run(runner(["clrst"]))
+    results = asyncio.run(runner(["crane"]))
     duration = time.time() - start_time
-    for result in results:
-        print()
-        print(result)
+    avgNarrowingScore = sum([r["narrowing_scores"] for r in results]) / len(results)
+    print(f"\nheavy narrowing score loop took {avgNarrowingScore} seconds")
 
     print(f"\nCalled in {duration} seconds")
-    # times = [x[1] for x in results]
-    # avg_process_time_per_guess = sum(times)/len(times)
-    # print(f"\nAvg process time: {avg_process_time_per_guess}")
