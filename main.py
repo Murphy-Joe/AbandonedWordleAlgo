efe3f488a8acf1cc5f24d77cad1eba4cff41fdec
Joe Murphy <murphcle@gmail.com>
2022-04-21 16:47:14 -0400

    set up cors for chrome extension

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from typing import Optional
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
 from game import WordleGame
@@ -10,6 +11,16 @@ from solver import Solver
 
 # pylint: disable=invalid-name
 app = FastAPI()
+origins = [
+    "https://www.nytimes.com",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["POST"],
+    allow_headers=["*"],
+)
 
 class Guesses(BaseModel):
     guesses: list[str]
