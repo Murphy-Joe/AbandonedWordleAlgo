97c4dd1c4be89b3a50381728f9297e923dd44c08
Joe Murphy <murphcle@gmail.com>
2022-04-21 07:59:55 -0400

    moved old synchronous code

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from typing import Optional
 
 from fastapi import FastAPI, Query
 from pydantic import BaseModel
-import aiohttp
+# import aiohttp
 
 from game import WordleGame
 from letter_middle import words_for_brute_force, letters_sorted_by_middleness
@@ -28,6 +28,7 @@ def targets_left(guesses: list[str]) -> list[str]:
 @app.post("/bestletters")
 async def best_letters(body: Guesses):
     words_left = targets_left(body.guesses)
+    return letters_sorted_by_middleness(words_left)
 
 @app.post("/wordsleft")
 async def root(body: Guesses):
@@ -36,20 +37,6 @@ async def root(body: Guesses):
     return {
         "wordsLeft": wordsLeft,
         "bestGuessWords": bestGuessWords,
-    }
-
-@app.get("/nextguesses")
-async def get_guess_dict(guesses: str = Query(str), nextguess: str = Query(str)):
-    guesses = guesses.split(',')
-    game = WordleGame()
-    for guess in guesses:
-        game.make_guess(guess)
-    game.make_guess(nextguess)
-    solver = Solver(game)
-    wordsLeft = solver.answers_that_meet_criteria(game.ResultsFilter)
-    guessDict = solver.narrowing_score_per_guess_async(nextguess, wordsLeft)
-    return {
-        "data": guessDict
     }
 
 @app.post("/game")
@@ -63,3 +50,20 @@ if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
 
     # uvicorn main:app --reload
+
+    #region old code
+    # synchronous version
+    # @app.get("/nextguesses")
+    # async def get_guess_dict(guesses: str = Query(str), nextguess: str = Query(str)):
+    #     guesses = guesses.split(',')
+    #     game = WordleGame()
+    #     for guess in guesses:
+    #         game.make_guess(guess)
+    #     game.make_guess(nextguess)
+    #     solver = Solver(game)
+    #     wordsLeft = solver.answers_that_meet_criteria(game.ResultsFilter)
+    #     guessDict = solver.narrowing_score_per_guess_async(nextguess, wordsLeft)
+    #     return {
+    #         "data": guessDict
+    #     }
+    # endregion
